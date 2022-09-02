9612800e31a14ae32d75a337ec887ddb5fc19b94
czechp <webcoderc@gmail.com>
2022-09-02 12:03:15 +0200

    Prepare docker scirpts

--- src/constant/URL.js
+++ src/constant/URL.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "host.docker.internal:9000";
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "host.docker.internal:9000";
+// export const BACKEND_URL = "http://localhost:8080";
