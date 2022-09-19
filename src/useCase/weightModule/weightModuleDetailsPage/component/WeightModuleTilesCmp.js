658a181d4cefc4800eb1f33aea6ecfa61034df05
czechp <webcoderc@gmail.com>
2022-09-19 13:29:29 +0200

    Tile section for weight modules details page

--- src/useCase/weightModule/weightModuleDetailsPage/component/WeightModuleTilesCmp.js
+++ src/useCase/weightModule/weightModuleDetailsPage/component/WeightModuleTilesCmp.js
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 import TileInfoCmp from "../../../../component/tile/TileInfoCmp";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPowerOff} from "@fortawesome/free-solid-svg-icons";
+import colors from "../../../../configuration/style/colors";
 
 const WeightModuleTilesCmp = ({weightModule}) => {
+    console.log(weightModule)
     return <Container>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
-        <TileInfoCmp></TileInfoCmp>
+        <TileInfoCmp title="Linia:" fontSize="large">{weightModule.productionLineName}</TileInfoCmp>
+        <TileInfoCmp title="Stan:">
+            <FontAwesomeIcon
+                icon={faPowerOff}
+                size={"8x"}
+                color={weightModule.moduleStatus.status ? colors.success : colors.danger}/>
+        </TileInfoCmp>
+        <TileInfoCmp title="Poprawnych pomiarów:"
+                     fontSize="middle">{weightModule.productionIndicators.correctProductPercent.toFixed(1) + "  %"}</TileInfoCmp>
+        <TileInfoCmp title="Waga produktu:"
+                     fontSize="middle">{`${weightModule.productInfo.downRangeWeight.toFixed(1)}-${weightModule.productInfo.upRangeWeight.toFixed(1)} g`}</TileInfoCmp>
+        <TileInfoCmp title="Ilość parafiny razem:"
+                     fontSize="middle">{weightModule.moduleStatus.currentDosingDevice.toFixed(1) + " kg"}</TileInfoCmp>
+        <TileInfoCmp title="Ilość parafiny razem:"
+                     fontSize="middle">{weightModule.productionIndicators.totalProductPcs + " szt."}</TileInfoCmp>
+        <TileInfoCmp title="Aktualny pomiar:"
+                     fontSize="middle">{weightModule.moduleStatus.currentMeasure + " g"}</TileInfoCmp>
+        <TileInfoCmp title="Aktualny rząd:"
+                     fontSize="middle">{weightModule.moduleStatus.currentDosingDevice}</TileInfoCmp>
+
+
+
     </Container>
 }
 
