989a30e30e1c6c29b30c7437292317ecee864430
czechp <webcoderc@gmail.com>
2022-09-19 12:09:36 +0200

    Navigation to module details page

--- src/useCase/weightModule/weightModuleDetailsPage/WeightModuleDetailsPage.js
+++ src/useCase/weightModule/weightModuleDetailsPage/WeightModuleDetailsPage.js
@@ -1,7 +1,14 @@
 import PageCmp from "../../../component/PageCmp";
+import {useLocation} from "react-router-dom";
+import GetRequestService from "../../../service/http/getRequestService";
 
 const WeightModuleDetailsPage = () => {
-    return <PageCmp title="Moduł wagowy -  szczegóły">
+    const URL = "/api/weight-modules";
+    const getRequestService = new GetRequestService();
+    const {state: weightModuleId} = useLocation();
+    const {object:weightModule} = getRequestService.getObject(`${URL}/${weightModuleId}`);
+
+    return <PageCmp title="Moduł wagowy -  szczegóły" loaded={weightModule}>
 
     </PageCmp>
 }
