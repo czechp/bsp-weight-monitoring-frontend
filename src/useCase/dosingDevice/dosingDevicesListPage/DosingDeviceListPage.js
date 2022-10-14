26eb930c1273fb18aa871020c40e625be309c273
czechp <webcoderc@gmail.com>
2022-10-14 13:46:27 +0200

    Added table with dosing devices for last modules

--- src/useCase/dosingDevice/dosingDevicesListPage/DosingDeviceListPage.js
+++ src/useCase/dosingDevice/dosingDevicesListPage/DosingDeviceListPage.js
@@ -6,11 +6,15 @@ const DosingDeviceListPage = () => {
     const getRequestService = new GetRequestService();
     const URL_FIRST_MODULES = "/api/dosing-devices/first";
     const {objects: dosingDevicesFirst, setSortingField: setSortingForFirst} = getRequestService.getObjectsArray(URL_FIRST_MODULES);
+    const URL_LAST_MODULES = "/api/dosing-devices/last";
+    const {objects: dosingDevicesLast, setSortingField: setSortingForLast} = getRequestService.getObjectsArray(URL_LAST_MODULES);
 
-    const dataLoaded = dosingDevicesFirst;
+
+    const dataLoaded = dosingDevicesFirst && dosingDevicesLast;
 
     return <PageCmp title="Dysze dozujące" loaded={dataLoaded}>
                 <DosingDevicesListCmp dosingDevices={dosingDevicesFirst} setSorting={setSortingForFirst} title={"Moduł I"} />
+                <DosingDevicesListCmp dosingDevices={dosingDevicesLast} setSorting={setSortingForLast} title={"Moduł II"} />
     </PageCmp>
 }
 
