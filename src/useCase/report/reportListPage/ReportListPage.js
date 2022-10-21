8e2b5a052c0cabaf66f21349d2c9a10cd2e6ff93
czechp <webcoderc@gmail.com>
2022-10-21 12:29:38 +0200

    Component to display and sorting reports list

--- src/useCase/report/reportListPage/ReportListPage.js
+++ src/useCase/report/reportListPage/ReportListPage.js
@@ -1,7 +1,14 @@
 import PageCmp from "../../../component/PageCmp";
+import ReportListCmp from "./component/ReportListCmp";
+import GetRequestService from "../../../service/http/getRequestService";
 
 const ReportListPage = () => {
-    return <PageCmp title="Lista raportów"></PageCmp>
+    const URL = "/api/reports";
+    const getRequestService = new GetRequestService();
+    const {objects: reports, setSortingField} = getRequestService.getObjectsArray(URL);
+    return <PageCmp title="Lista raportów" loaded={reports}>
+        <ReportListCmp reports={reports} setSortingField={setSortingField}/>
+    </PageCmp>
 }
 
 export default ReportListPage;
