b486aabb41372cc670d5d96f1b463132c5589544
czechp <webcoderc@gmail.com>
2022-10-21 13:34:00 +0200

    Tiles with basic data about report

--- src/useCase/report/reportDetailsPage/ReportDetailsPage.js
+++ src/useCase/report/reportDetailsPage/ReportDetailsPage.js
@@ -1,9 +1,18 @@
 import PageCmp from "../../../component/PageCmp";
 import {useLocation} from "react-router-dom";
+import GetRequestService from "../../../service/http/getRequestService";
+import ReportDetailsTilesCmp from "./component/ReportDetailsTilesCmp";
 
 const ReportDetailsPage = () => {
     const {state: reportId} = useLocation();
-    return <PageCmp title="Szczegóły raportu">
+    const URL = `/api/reports/${reportId}`;
+    const getRequestService = new GetRequestService();
+    const {object: report} = getRequestService.getObject(URL);
+
+    return <PageCmp title="Szczegóły raportu" loaded={report}>
+        {report && <>
+            <ReportDetailsTilesCmp report={report} />
+        </>}
     </PageCmp>
 }
 
